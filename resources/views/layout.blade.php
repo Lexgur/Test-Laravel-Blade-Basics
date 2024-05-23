f561a83c3c2cb08af8c5368bd8fa27eef7fcddc3
Lexgur <edgaras.malukas@gmail.com>
2024-05-23 23:48:16 +0300

    Testing if it's good

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main') {{-- Extend the main layout --}}
+
+@section('content') {{-- Define the content section --}}
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                {{-- Your content goes here --}}
+                Please change layout.
+            </div>
+        </div>
+    </div>
+</div>
+@endsection {{-- End of content section --}}
